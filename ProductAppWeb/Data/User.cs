c7f8f27d0db2270f6a8b0700e46d8bafd452bf5f
Valdemar-iatneh <avvakumov112003@gmail.com>
2022-07-02 11:27:14 +0300

    add files

---
+++ ProductAppWeb/Data/User.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace ProductAppWeb.Data
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public static List<User> st = new List<User>();
+
+        public User(string name, string login, string mail)
+        {
+            this.Name = name;
+            this.Login = login;
+            this.Password = mail;
+        }
+
+        public User()
+        {
+
+        }
+
+        public static void AddClient(string name, string login, string password)
+        {
+            User std = new User(name, login, password);
+            st.Add(std);
+        }
+
+        public static List<User> GetClient()
+        {
+            return st;
+        }
+    }
+}
